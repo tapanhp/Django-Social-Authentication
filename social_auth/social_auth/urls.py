342be4fb018c12494ae34a4be608a3b65d1c190c
TruthSeekerBeast <truthseekerbeast@gmail.com>
2020-01-03 18:06:38 +0530

    Implemented social authentication with facebook, google and github

--- social_auth/social_auth/urls.py
+++ social_auth/social_auth/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
